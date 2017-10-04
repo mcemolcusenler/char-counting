6eda6587409f42ae15910ff7852dfde898218221
mcemolcusenler <cem.olcusenler@gmail.com>
2017-10-04 17:36:49 +0000

    Final Edition

---
+++ char-counting.js
@@ -0,0 +1,13 @@
+function countLetters() {
+  var obj = {};
+  inputArray.forEach(function(e) {
+    if (!(e in obj)) {
+      obj[e] = 0;
+    }
+    obj[e] += 1;
+  })
+  console.log(obj);
+}
+
+var inputArray = process.argv[2].split('');
+countLetters();
